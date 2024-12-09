5c204bec08c8ea40fc7e7414622637bf0b15de88
n.legay <nicolas.legay@hafa.fr>
2024-12-09 11:39:46 +0100

    test zutocert

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"hephaistos/server"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("hephaistos.hafa.fr"), //Your domain here
+		Cache:      autocert.DirCache("./certs"),                 //Folder for storing certificates
+	}
 	tls_cfg := tls.Config{
 		MinVersion:               tls.VersionTLS12,
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
@@ -18,6 +25,7 @@ func main() {
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
+		GetCertificate: certManager.GetCertificate,
 	}
 	s := &server.Server{
 		Config: http.Server{
@@ -26,7 +34,7 @@ func main() {
 			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 		},
 	}
-
+	s.Config.TLSConfig.NextProtos = append([]string{"h2", "http/1.1"}, s.Config.TLSConfig.NextProtos...)
 	s.HandleRoutes()
 
 	log.Println("Listening on https://localhost:8443/")
