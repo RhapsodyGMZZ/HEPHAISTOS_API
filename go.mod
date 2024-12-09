d7fae1c281c261351cddf7ad5adc1404f1f12f34
n.legay <nicolas.legay@hafa.fr>
2024-12-09 11:52:05 +0100

    testing

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module hephaistos
 
 go 1.23.4
+
+require (
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,19 +5,18 @@ import (
 	"hephaistos/server"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("hephaistos.hafa.fr"),
+		Cache:      autocert.DirCache("certs"),
+	}
 	tls_cfg := tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
+		GetCertificate: certManager.GetCertificate,
 	}
 	s := &server.Server{
 		Config: http.Server{
@@ -27,9 +26,9 @@ func main() {
 		},
 	}
 	s.HandleRoutes()
-
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
 	log.Println("Listening on https://localhost:8443/")
-	if err := s.Config.ListenAndServeTLS("./certs/server.crt", "./certs/server.key"); err != nil {
+	if err := s.Config.ListenAndServeTLS("", ""); err != nil {
 		log.Println(err)
 		log.Fatal("Can't establish connection")
 	}
